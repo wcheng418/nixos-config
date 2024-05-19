a1ccdfb3a95370dfdacc1ad210a8bbe10e2a50fb
Pravin Ramana <pravinramana25@protonmail.ch>
2024-05-19 16:18:25 -0500

    Update proton_launcher.sh

--- bwrap/proton_launcher.sh
+++ bwrap/proton_launcher.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# Function to restore the original scale of all outputs
+Function to restore the original scale of all outputs
 restore_scale() {
   printf '%s\n' "$outputs" | while read -r name scale; do
     swaymsg output "$name" scale "$scale"
   done
 }
-file_path=$1
-directory=$(dirname "$file_path")
 
-cd "$directory" || exit
+file_path=$1
+directory=$(realpath "$(dirname "$file_path")")
 
 # Trap signals to ensure that the original scale is always restored
 trap restore_scale TERM
@@ -22,29 +21,32 @@ swaymsg output "*" scale 1
 
 # Run the game
 bwrap \
-    --unshare-all \
-    --share-net \
-    --die-with-parent \
-    --proc /proc \
-    --tmpfs /tmp \
-    --dev-bind /dev /dev \
-    --ro-bind /sys /sys \
-    --ro-bind /var /var \
-    --ro-bind /run /run \
-    --ro-bind /etc /etc \
-    --ro-bind /usr/share /usr/share \
-    --ro-bind /usr/bin /usr/bin \
-    --ro-bind /usr/lib /usr/lib \
-    --ro-bind /usr/lib32 /usr/lib32 \
-    --symlink /usr/bin /bin \
-    --symlink /usr/lib /lib \
-    --symlink /usr/lib /lib64 \
-    --symlink /usr/bin /sbin \
-    --ro-bind "$directory" "$directory" \
-    --bind ~/.cache/protonfixes ~/.cache/protonfixes \
-    --bind ~/.local/share/proton-pfx ~/.local/share/proton-pfx \
-    --bind ~/.cache/dxvk-cache-pool ~/.cache/dxvk-cache-pool \
-    proton "$@"
+  --unshare-all \
+  --share-net \
+  --die-with-parent \
+  --proc /proc \
+  --tmpfs /tmp \
+  --mqueue /dev/mqueue \
+  --dev-bind /dev /dev \
+  --ro-bind /sys /sys \
+  --ro-bind /var /var \
+  --ro-bind /run /run \
+  --ro-bind /etc /etc \
+  --ro-bind /usr/share /usr/share \
+  --ro-bind /usr/bin /usr/bin \
+  --ro-bind /usr/lib /usr/lib \
+  --ro-bind /usr/lib32 /usr/lib32 \
+  --symlink /usr/bin /bin \
+  --symlink /usr/lib /lib \
+  --symlink /usr/lib /lib64 \
+  --symlink /usr/bin /sbin \
+  --bind ~/.local/share/proton-pfx ~/.local/share/proton-pfx \
+  --bind ~/.cache/dxvk-cache-pool ~/.cache/dxvk-cache-pool \
+  --bind ~/.cache/protonfixes ~/.cache/protonfixes \
+  --bind ~/Games ~/Games \
+  --ro-bind "$directory" "$directory" \
+  --chdir "$directory" \
+  /usr/bin/proton "$@"
 
 # Restore the original scale of all outputs
 restore_scale
